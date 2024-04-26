abaa39ae9ae02e73deb5b43faca7ff35ac2f3aaf
darylboddie <dboddie5@students.kennesaw.edu>
2024-04-26 11:38:07 -0400

    updating code

--- FoodExpenses.py
+++ FoodExpenses.py
@@ -1,7 +1,7 @@
-import sqlite3
+import sqlite3 
 # Create a database
 def init():
-    conn = db.connect("spent.db")
+    conn = sqlite3.connect("spent.db")
     cur = conn.cursor()
     sql = '''
     create table if not exists expenses (
@@ -19,7 +19,7 @@ init()
 def log(amount, category, message=""):
     from datetime import datetime
     date = str(datetime.now())
-    conn = db.connect("spent.db")
+    conn = sqlite3.connect("spent.db")
     cur = conn.cursor()
     sql = '''
     insert into expenses values (
@@ -39,7 +39,7 @@ def log(amount, category, message=""):
 
 # View expenses for month and category 
 def view(date,category):
-    conn = db.connect("spent.db")
+    conn = sqlite3.connect("spent.db")
     cur = conn.cursor()
     if category.isalpha():
         sql = '''
@@ -59,7 +59,7 @@ def view(date,category):
 
 # Create introduction and instructions to explain the purpose of the code
 print("\nWelcome to Monthly Food Expenses!")
-print("View the total amount you have spent in a month for food")
+print("View the total amount you have spent in a month")
 
 # Gather user input to use different functions if the input is true
 while True:
@@ -72,9 +72,9 @@ while True:
         print('Database initialized')
     if ans == "2":
         cost = input("What is the amount spent?\n: ")
-        cat = input("Enter the expense category\n: ")
+        category = input("Enter the expense category\n: ")
         msg = input("Where was the amount spent?\n: ")
-        log(cost,cat,msg)
+        log(cost,category,msg)
     elif ans == "3":
         date = input("Enter the date (yyyy-mm)\n: ")
         category = input("Enter the category you would like to review or press enter to view all expenses\n: ").title()
